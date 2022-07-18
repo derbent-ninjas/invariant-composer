e561b27352653a6a4c01e4f5ea2f3cdd7cc5d7dd
TbotaPhantA <botakiller@mail.ru>
2022-07-18 19:10:39 +0000

    invariant Either was created and covered with tests

---
+++ src/utils/invariant.ts
@@ -0,0 +1,54 @@
+export interface InvariantFail {
+  readonly data: {
+    readonly failMessage: string,
+    [key: string]: any;
+  }
+  readonly path?: string;
+}
+
+export interface Success {
+  readonly _tag: 'Success';
+}
+
+export interface Fail {
+  readonly _tag: 'Fail';
+  readonly fail: InvariantFail;
+}
+
+export type Invariant = Success | Fail;
+
+/**
+ * Creates Success Invariant;
+ */
+export const success = (): Success => ({
+  _tag: 'Success',
+} as const)
+
+/**
+ * Creates Fail Invariant;
+ */
+export const fail = (failData: InvariantFail['data']): Fail => ({
+  _tag: 'Fail',
+  fail: {
+    data: failData,
+  }
+} as const)
+
+/**
+ * If invariant is Success:
+ * - returns true
+ * - asserts invariant type as Success
+ * @param invariant
+ */
+export const isSuccess = (invariant: Invariant): invariant is Success =>
+  invariant._tag === 'Success';
+
+/**
+ * If invariant is Fail:
+ * - returns true
+ * - asserts invariant type as Success
+ * @param invariant
+ */
+export const isFail = (invariant: Invariant): invariant is Fail =>
+  invariant._tag === 'Fail';
+
---
+++ tests/utils/invariant.spec.ts
@@ -0,0 +1,34 @@
+import { fail, isFail, isSuccess, success } from '../../src/utils/invariant';
+
+describe('Invariant', () => {
+  const testCases = [
+    {
+      toString: () => 'isSuccess for Success - should return true',
+      predicate: isSuccess,
+      invariant: success(),
+      expectedPredicateResult: true,
+    },
+    {
+      toString: () => 'isFail for Fail - should return true',
+      predicate: isFail,
+      invariant: fail({ failMessage: 'some message' }),
+      expectedPredicateResult: true,
+    },
+    {
+      toString: () => 'isSuccess for Fail - should return false',
+      predicate: isSuccess,
+      invariant: fail({ failMessage: 'some message' }),
+      expectedPredicateResult: false,
+    },
+    {
+      toString: () => 'isFail for Success - should return false',
+      predicate: isFail,
+      invariant: success(),
+      expectedPredicateResult: false,
+    },
+  ]
+
+  test.each(testCases)('%s', ({ predicate, invariant, expectedPredicateResult }) => {
+    expect(predicate(invariant)).toStrictEqual(expectedPredicateResult);
+  })
+});
